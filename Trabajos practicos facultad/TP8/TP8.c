c1e298edd82a2f2b72a6a3096f7eb9f71fa614ad
unknown <wenceslaovillegas14@gmail.com>
2022-09-07 15:26:07 -0300

    punto 8

---
+++ Trabajos practicos facultad/TP8/TP8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// Dado el precio de un producto indicar, por pantalla, los impuestos que debe pagar. IVA 21%, Ingresos Brutos 2.5% e Impuesto Municipal 1.5%
+// corregir
+int main () {
+    float precio, iva, IB, IM;
+    printf("Ingrese el precio del producto\n");
+    scanf("%d", &precio);
+    iva = (precio * 1.21);
+    IB = (precio * 1.025);
+    iva = (precio * 1.015);
+    printf("El precio es: %d\n", precio);
+    printf("El precio mas IVA es: %d\n", iva);
+    printf("El precio mas IB es: %d\n", IB);
+    printf("El precio mas IM es: %d\n", IM);
+    return 0;
+}
